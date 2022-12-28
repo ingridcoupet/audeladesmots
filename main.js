2a96cdc105d54ec112785fd0c1214691205e373f
ingridcoupet <120275745+ingridcoupet@users.noreply.github.com>
2022-12-28 14:34:49 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//  BURGER MENU
+
 const menuHamburger = document.getElementById("menu-hamburger")
 const navLinks = document.getElementById("nav-links")
 const body = document.body
@@ -17,4 +19,53 @@ function activate(activer) {
     document.getElementById("scroll4").classList.remove("active")
     document.getElementById("scroll5").classList.remove("active")
     document.getElementById(activer).classList.add("active")
+}
+
+//  DIAPO
+
+let compteur = 0;
+let timer, elements, slides, slideWidth, speed, transition;
+
+window.onload = () => {
+    const diapo = document.querySelector(".diapo");
+    speed = diapo.dataset.speed * 2;
+    
+    transition = diapo.dataset.transition;
+
+    elements = document.querySelector(".elements");
+
+    let firstImage = elements.firstElementChild.cloneNode(true);
+
+    elements.appendChild(firstImage);
+
+    slides = Array.from(elements.children);
+
+    slideWidth = diapo.getBoundingClientRect().width;
+
+    timer = setInterval(slideNext, speed);
+}
+
+window.onresize = () => {
+    const diapo = document.querySelector(".diapo");
+    slideWidth = diapo.getBoundingClientRect().width;
+}
+
+function slideNext(){
+    compteur++;
+    elements.style.transition = transition+"ms linear";
+
+    let decal = -slideWidth * compteur;
+    elements.style.transform = `translateX(${decal}px)`;
+
+    setTimeout(function(){
+        if(compteur >= slides.length - 1){
+            compteur = 0;
+            elements.style.transition = "unset";
+            elements.style.transform = "translateX(0)";
+        }
+    }, transition);
+}
+
+function startTimer(){
+    timer = setInterval(slideNext, speed);
 }
