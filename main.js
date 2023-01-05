c114a94f40e9c60af7eaca228c95de7788f92f95
ingridcoupet <120275745+ingridcoupet@users.noreply.github.com>
2023-01-05 18:21:31 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -67,3 +67,29 @@ function slideNext(){
 function startTimer(){
     timer = setInterval(slideNext, speed);
 }
+
+// TARIFS
+
+let requestURL = 'prices.json'
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function heroes() {
+    const prices = request.response;
+    printAllPrices(prices)
+}
+
+function printAllPrices(prices) {
+    for (let i = 0; i < prices.length; i++) {
+        var dir = document.getElementById("price-container");
+        var newPriceClass = document.createElement("div");
+        var priceTitle = prices[i].title;
+        var priceImg = prices[i].img;
+        var priceDesc = prices[i].desc;
+        var priceCost = prices[i].cost;
+        newPriceClass.classList.add("price-box");
+        newPriceClass.innerHTML = `<h1 class="price-title" id="price1">${priceTitle}</h1><img src="${priceImg}" alt="Image de l\'offre ${prices[i].id}" class="price-image"><p class="price-desc">${priceDesc}</p><p class="price-cost">${priceCost}</p>`;
+        dir.appendChild(newPriceClass);
+    }
+}
